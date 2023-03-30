347d3d723ba500021441a6eaa2b405189e159032
alliemcclure23 <alliemcclure23@gmail.com>
2023-03-29 21:09:51 -0400

    created country class

---
+++ app/src/main/java/edu/uga/countryquiz/country.java
@@ -0,0 +1,18 @@
+package edu.uga.countryquiz;
+
+public class country {
+    String countryName;
+    String ContinentName;
+
+    public country()
+    {
+    }
+    public void setContinentName(String continentName) {
+        ContinentName = continentName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+}
+
