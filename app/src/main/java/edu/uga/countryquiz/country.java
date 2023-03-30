c521c851393f31a3c87a8b369555c243ed7bac53
alliemcclure23 <alliemcclure23@gmail.com>
2023-03-29 21:11:42 -0400

    javadoc comments on country class

--- app/src/main/java/edu/uga/countryquiz/country.java
+++ app/src/main/java/edu/uga/countryquiz/country.java
@@ -4,13 +4,21 @@ public class country {
     String countryName;
     String ContinentName;
 
+    /* creates a country object*/
     public country()
     {
     }
+
+    /*sets the continent name
+    * param string
+    */
     public void setContinentName(String continentName) {
         ContinentName = continentName;
     }
 
+    /*sets the country name
+     * param string
+     */
     public void setCountryName(String countryName) {
         this.countryName = countryName;
     }
