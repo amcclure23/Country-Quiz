8d73a1b0f66fa2f7ff72e02327d6821737a1d410
alliemcclure23 <alliemcclure23@gmail.com>
2023-04-07 19:56:25 -0400

    fixed quiz fragment

--- app/src/main/java/edu/uga/countryquiz/QuizFragment.java
+++ app/src/main/java/edu/uga/countryquiz/QuizFragment.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
+import java.util.Random;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +19,7 @@ import android.widget.TextView;
  */
 public class QuizFragment extends Fragment {
 
+    public String[] continents = new String[]{"Africa","Antarctica", "Asia", "Australia", "Europe", "North America", "South America"};
     private int questionNum;
     private String title;
     private int page;
@@ -27,23 +28,21 @@ public class QuizFragment extends Fragment {
     private RadioButton button1;
     private RadioButton button2;
     private RadioButton button3;
-    private String answer;
+    private int answer = 0;
+    private String questionWord;
+    private String correctAnswer;
+
     public QuizFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment QuizFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static QuizFragment newInstance(int page, String title) {
+    public static QuizFragment newInstance(int page, String title, String questionWord, String correctAnswer) {
         QuizFragment fragment = new QuizFragment();
         Bundle args = new Bundle();
         args.putInt("someInt", page);
         args.putString("someTitle", title);
+        args.putString("questionWord", questionWord);
+        args.putString("correctAnswer", correctAnswer);
         fragment.setArguments(args);
         return fragment;
     }
@@ -54,6 +53,8 @@ public class QuizFragment extends Fragment {
         if (getArguments() != null) {
             page = getArguments().getInt("someInt", 0);
             title = getArguments().getString("someTitle");
+            questionWord = getArguments().getString("questionWord", questionWord);
+            correctAnswer= getArguments().getString("correctAnswer", correctAnswer);
         }
     }
 
@@ -83,10 +84,11 @@ public class QuizFragment extends Fragment {
                                                   {
                                                       // Get the selected Radio Button
                                                       RadioButton radioButtonPicked = (RadioButton)group.findViewById(checkedId);
-                                                      answer =  radioButtonPicked.getText().toString();
+                                                      if(correctAnswer.equals(radioButtonPicked.getText().toString())){
+                                                          answer = 1;
+                                                      }
                                                   }
                                               });
-       
     }
 
     private void setText(View view) {
@@ -94,10 +96,40 @@ public class QuizFragment extends Fragment {
         button1  = view.findViewById( R.id.radioButton);
         button2  = view.findViewById( R.id.radioButton2);
         button3  = view.findViewById( R.id.radioButton3);
-       // textView.setText(newQuiz.quest[page-1].questionWord);
-       //button1 .setText(newQuiz.quest[page-1].correctAnswer);
-       // button1 .setText(newQuiz.quest[page-1].wrongAnswer1);
-      //  button1 .setText(newQuiz.quest[page-1].wrongAnswer2);
+        textView.setText(questionWord);
+        Random rand = new Random();
+        String[] answers = new String[3];
+        answers[0]=correctAnswer;
+        int int_random = rand.nextInt(7);
+
+        while(correctAnswer.equals(continents[int_random])){
+            int_random = rand.nextInt(7);
+        }
+        answers[1]=continents[int_random];
+        while(correctAnswer.equals(continents[int_random])||answers[1].equals(continents[int_random])){
+            int_random = rand.nextInt(7);
+        }
+        answers[2]=continents[int_random];
+        int_random = rand.nextInt(3);
+        button1 .setText(answers[int_random]);
+        if(int_random == 2)
+        {
+            int_random = 0;
+        }
+        else
+        {
+            int_random++;
+        }
+        button2 .setText(answers[int_random]);
+        if(int_random == 2)
+        {
+            int_random = 0;
+        }
+        else
+        {
+            int_random++;
+        }
+      button3 .setText(answers[int_random]);
     }
 
 }
